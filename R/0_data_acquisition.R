3c34043ba13c9d1891f0926252fb25ebd06f843f
brunomc-eco <brunomc.eco@gmail.com>
2020-06-03 13:29:48 -0300

    ocupacao de leitos MS

---
+++ R/0_data_acquisition.R
@@ -0,0 +1,9 @@
+# COVID-19 Dashboard for IBGE
+# Bruno Carvalho, brunomc.eco@gmail.com
+# Downloading raw data from public sources
+
+# ocupacao de leitos, Ministério da Saúde
+# https://gestaoleitos.saude.gov.br/app/kibana#/dashboard/18685ad0-81b4-11ea-9f02-bfa3f55b2dad
+# no final do dashboard, exportar arquivo "Formatted"
+# salvar na pasta "./data/leitos_MS"
+# renomear arquivo para "leitos_MS.csv"
